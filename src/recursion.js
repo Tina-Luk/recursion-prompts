0a7ec4a4e9db8910e496423e0704e473d67dea2f
Tina Luk <tina.luk.tina@gmail.com>
2020-06-20 11:19:39 -0700

    Completed problems through 20 except 14

--- src/recursion.js
+++ src/recursion.js
@@ -253,8 +253,14 @@ var reverseArr = function(array) {
 // 18. Create a new array with a given value and length.
 // buildList(0,5) // [0,0,0,0,0]
 // buildList(7,3) // [7,7,7]
+
 var buildList = function(value, length) {
-};
+    if (length === 0) {
+      return[];
+    } else {
+      return [value].concat(buildList(value, length - 1));
+    }
+  };
 
 // 19. Implement FizzBuzz. Given integer n, return an array of the string representations of 1 to n.
 // For multiples of three, output 'Fizz' instead of the number.
@@ -262,13 +268,37 @@ var buildList = function(value, length) {
 // For numbers which are multiples of both three and five, output “FizzBuzz” instead of the number.
 // fizzBuzz(5) // ['1','2','Fizz','4','Buzz']
 var fizzBuzz = function(n) {
+    let results = [];
+    if (n === 1) {
+        return ['1'];
+    } else {
+        if (n % 3 === 0 && n % 5 === 0) {
+            results.push('FizzBuzz');
+        } else if (n % 3 === 0) {
+            results.push('Fizz');
+        } else if (n % 5 === 0) {
+            results.push('Buzz');
+        } else {
+            results.push('' + n);
+        }
+    return fizzBuzz(n-1).concat(results);
+    }
 };
 
 // 20. Count the occurence of a value in a list.
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
 var countOccurrence = function(array, value) {
-};
+    let count = 0;
+    if (array.length === 0) {
+      return count;
+    }
+    if (array[0] === value) {
+      
+      return 1 + countOccurrence(array.slice(1), value);
+    }
+    return countOccurrence(array.slice(1), value);
+  };
 
 // 21. Write a recursive version of map.
 // rMap([1,2,3], timesTwo); // [2,4,6]
